05010729c4a69be890821abb2c031cc3df94c67b
Anton Feldmann <anton.feldmann>
2021-10-24 20:06:12 +0200

    crater training step. TODO: Debugging and test

--- cratergan/gan.py
+++ cratergan/gan.py
@@ -1,3 +1,5 @@
+from typing import OrderedDict
+
 import torch
 import torch.nn.functional as F
 
@@ -56,4 +58,52 @@ class CraterGAN(LightningModule):
         self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
 
     def training_step(self, batch, batch_idx, optimizer_idx):
-        pass
+        imgs, _ = batch
+
+        # sample noise
+        z = torch.randn(imgs.shape[0], self.hparams.latent_dim)
+        z = z.type_as(imgs)
+
+        # train generator
+        if optimizer_idx == 0:
+
+            # generate images
+            self.generated_imgs = self(z)
+
+            # log sampled images
+            sample_imgs = self.generated_imgs[:6]
+            grid = torchvision.utils.make_grid(sample_imgs)
+            self.logger.experiment.add_image("generated_images", grid, 0)
+
+            # ground truth result (ie: all fake)
+            # put on GPU because we created this tensor inside training_loop
+            valid = torch.ones(imgs.size(0), 1)
+            valid = valid.type_as(imgs)
+
+            # adversarial loss is binary cross-entropy
+            g_loss = self.adversarial_loss(self.discriminator(self(z)), valid)
+            tqdm_dict = {"g_loss": g_loss}
+            output = OrderedDict({"loss": g_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            return output
+
+        # train discriminator
+        if optimizer_idx == 1:
+            # Measure discriminator's ability to classify real from generated samples
+
+            # how well can it label as real?
+            valid = torch.ones(imgs.size(0), 1)
+            valid = valid.type_as(imgs)
+
+            real_loss = self.adversarial_loss(self.discriminator(imgs), valid)
+
+            # how well can it label as fake?
+            fake = torch.zeros(imgs.size(0), 1)
+            fake = fake.type_as(imgs)
+
+            fake_loss = self.adversarial_loss(self.discriminator(self(z).detach()), fake)
+
+            # discriminator loss is the average of these
+            d_loss = (real_loss + fake_loss) / 2
+            tqdm_dict = {"d_loss": d_loss}
+            output = OrderedDict({"loss": d_loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            return output
